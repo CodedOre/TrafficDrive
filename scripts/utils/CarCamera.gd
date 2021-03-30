15cb75508dbfc92b5eadc5a503a755f7e8b32aa4
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-03-30 13:10:50 +0200

    CarCamera: Add basic reset

--- scripts/utils/CarCamera.gd
+++ scripts/utils/CarCamera.gd
@@ -7,6 +7,11 @@ extends Spatial
 
 # -- Properties --
 
+# - Camera position -
+export (float) var CameraDistance = 4.5
+export (float) var CameraHeight   = 1
+export (int)   var CameraAngle    = 15
+
 # - Active camera indicator -
 export (bool) var current = false setget set_current, is_current
 
@@ -17,31 +22,39 @@ onready var _camera_node = $InnerGimbal/Camera
 onready var _gimbal_node = $InnerGimbal
 
 # - Internal variables -
-onready var x_dir             =   -1 if GameSettings.get_setting("Input", "MouseXInverted") else  1
-onready var y_dir             =    1 if GameSettings.get_setting("Input", "MouseYInverted") else -1
-onready var mouse_sensitivity = 0.001 * GameSettings.get_setting("Input", "MouseSensitivity")
+onready var _x_dir             =   -1 if GameSettings.get_setting("Input", "MouseXInverted") else  1
+onready var _y_dir             =    1 if GameSettings.get_setting("Input", "MouseYInverted") else -1
+onready var _mouse_sensitivity = 0.001 * GameSettings.get_setting("Input", "MouseSensitivity")
+var _original_outer_transform : Transform
+var _original_inner_transform : Transform
 
 # -- Functions --
 
 # - Run at startup -
 func _ready():
 	GameSettings.connect("setting_changed", self, "_modify_settings")
-	pass
+	_original_outer_transform = transform
+	_original_inner_transform = _gimbal_node.transform
 
 # - Change internal variables when settings were modified -
 func _modify_settings():
-	x_dir             =   -1 if GameSettings.get_setting("Input", "MouseXInverted") else  1
-	y_dir             =    1 if GameSettings.get_setting("Input", "MouseYInverted") else -1
-	mouse_sensitivity = 0.001 * GameSettings.get_setting("Input", "MouseSensitivity")
+	_x_dir             =   -1 if GameSettings.get_setting("Input", "MouseXInverted") else  1
+	_y_dir             =    1 if GameSettings.get_setting("Input", "MouseYInverted") else -1
+	_mouse_sensitivity = 0.001 * GameSettings.get_setting("Input", "MouseSensitivity")
 
 # - Moves the camera according to mouse input -
 func _input(event):
 	if is_current():
 		if event is InputEventMouseMotion:
 			if event.relative.x != 0:
-				rotate_object_local(Vector3.UP, y_dir * event.relative.x * mouse_sensitivity)
+				rotate_object_local(Vector3.UP, _y_dir * event.relative.x * _mouse_sensitivity)
 			if event.relative.y != 0:
-				_gimbal_node.rotate_object_local(Vector3.RIGHT, x_dir * event.relative.y * mouse_sensitivity)
+				_gimbal_node.rotate_object_local(Vector3.RIGHT, _x_dir * event.relative.y * _mouse_sensitivity)
+
+# - Resets the camera to original position -
+func reset_camera():
+	transform = _original_outer_transform
+	_gimbal_node.transform = _original_inner_transform
 
 # - Active camera property -
 func set_current(value):
