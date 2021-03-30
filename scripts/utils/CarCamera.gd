786306fdddc1c7058240e3f65d99c3567483bf58
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-03-30 12:35:16 +0200

    CarCamera: Add Input Settings

--- scripts/utils/CarCamera.gd
+++ scripts/utils/CarCamera.gd
@@ -16,16 +16,32 @@ export (bool) var current = false setget set_current, is_current
 onready var _camera_node = $InnerGimbal/Camera
 onready var _gimbal_node = $InnerGimbal
 
+# - Internal variables -
+onready var x_dir             =   -1 if GameSettings.get_setting("Input", "MouseXInverted") else  1
+onready var y_dir             =    1 if GameSettings.get_setting("Input", "MouseYInverted") else -1
+onready var mouse_sensitivity = 0.001 * GameSettings.get_setting("Input", "MouseSensitivity")
+
 # -- Functions --
+
+# - Run at startup -
+func _ready():
+	GameSettings.connect("setting_changed", self, "_modify_settings")
+	pass
+
+# - Change internal variables when settings were modified -
+func _modify_settings():
+	x_dir             =   -1 if GameSettings.get_setting("Input", "MouseXInverted") else  1
+	y_dir             =    1 if GameSettings.get_setting("Input", "MouseYInverted") else -1
+	mouse_sensitivity = 0.001 * GameSettings.get_setting("Input", "MouseSensitivity")
 
 # - Moves the camera according to mouse input -
 func _input(event):
 	if is_current():
 		if event is InputEventMouseMotion:
 			if event.relative.x != 0:
-				rotate_object_local(Vector3.UP, -1 * event.relative.x * 0.002)
+				rotate_object_local(Vector3.UP, y_dir * event.relative.x * mouse_sensitivity)
 			if event.relative.y != 0:
-				_gimbal_node.rotate_object_local(Vector3.RIGHT, 1 * event.relative.y * 0.002)
+				_gimbal_node.rotate_object_local(Vector3.RIGHT, x_dir * event.relative.y * mouse_sensitivity)
 
 # - Active camera property -
 func set_current(value):
