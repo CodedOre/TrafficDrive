eaf36dacfdd0b70d5d66e2813692bd9af8bb18a2
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-03-31 14:26:53 +0200

    Vehicle: Add Input for Nightlights

--- scripts/vehicles/Vehicle.gd
+++ scripts/vehicles/Vehicle.gd
@@ -11,6 +11,7 @@ class_name Vehicle
 # - NodePaths from the vehicle -
 export (Array, NodePath) onready var Lights
 export (NodePath)        onready var Camera
+export (bool)                    var Controlled
 
 # -- Variables --
 
@@ -21,10 +22,27 @@ var _camera_node   : CameraPoint
 
 # -- Functions --
 
-# - Run at startup -
+# - Runs at startup -
 func _ready():
 	# Initialize VehicleLightManager
 	for path in Lights:
 		_light_nodes.append(get_node(path))
 	_light_manager = VehicleLightsManager.new(self, _light_nodes)
 	_camera_node = get_node(Camera)
+
+# - Runs at every frame -
+func _physics_process(_delta : float):
+	if Controlled:
+		_manage_input()
+
+# - Checks input at every frame -
+func _manage_input():
+	# Input for Nightlights
+	if Input.is_action_just_pressed("vehicle_nightlight_toggle"):
+		match _light_manager.NightLights:
+			VehicleLightsManager.NightLightMode.OFF:
+				_light_manager.NightLights = VehicleLightsManager.NightLightMode.ON
+			VehicleLightsManager.NightLightMode.ON:
+				_light_manager.NightLights = VehicleLightsManager.NightLightMode.FAR
+			VehicleLightsManager.NightLightMode.FAR:
+				_light_manager.NightLights = VehicleLightsManager.NightLightMode.OFF
