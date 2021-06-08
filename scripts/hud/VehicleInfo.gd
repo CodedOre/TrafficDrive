d6533d664352a91d863a3093aceede35948a4d63
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-06-08 15:36:20 +0200

    Make the Needle spin!
    Now we have a functional RPM display, but not a good RPM generation...

--- scripts/hud/VehicleInfo.gd
+++ scripts/hud/VehicleInfo.gd
@@ -10,6 +10,10 @@ extends MarginContainer
 # - RPM Range constant -
 const MAX_DISPLAYED_RPM : int = 8000
 
+# - Needle contants -
+const MIN_NEEDLE_DEGREE : int = -125
+const MAX_NEEDLE_DEGREE : int =  125
+
 # -- Properties --
 
 var DisplayedVehicle : Vehicle setget set_debug_vehicle, get_debug_vehicle
@@ -20,6 +24,7 @@ var DisplayedVehicle : Vehicle setget set_debug_vehicle, get_debug_vehicle
 onready var _speed_field : Label           = $Background/SpeedValue
 onready var _gear_field  : Label           = $Background/GearValue
 onready var _rpm_range   : TextureProgress = $RPMRange
+onready var _rpm_needle  : TextureRect     = $Needle
 
 # - Runtime variables -
 var _vehicle_connected : bool
@@ -30,8 +35,16 @@ var _displayed_vehicle : Vehicle
 # - Runs at every frame -
 func _physics_process(_delta):
 	if _displayed_vehicle != null:
+		# Update text fields
 		_speed_field.text = str(_displayed_vehicle.current_speed)
 		_gear_field.text  = _displayed_vehicle.GearsIdentifier[_displayed_vehicle._current_gear]
+		
+		# Update RPM needle
+		var vehicle_rpm  : float = float(_displayed_vehicle._engine_rpm)
+		var rpm_ratio    : float = vehicle_rpm / float(MAX_DISPLAYED_RPM)
+		var spin_degree  : int   = MAX_NEEDLE_DEGREE - MIN_NEEDLE_DEGREE
+		var needle_ratio : float = float(spin_degree) * rpm_ratio
+		_rpm_needle.rect_rotation = MIN_NEEDLE_DEGREE + needle_ratio
 
 # - DisplayedVehicle property -
 func set_debug_vehicle(object : Vehicle) -> void:
@@ -40,8 +53,6 @@ func set_debug_vehicle(object : Vehicle) -> void:
 	var vehicle_max_rpm : float = float(_displayed_vehicle.MaxEngineRPM)
 	var max_rpm_ratio   : float = (vehicle_max_rpm / float(MAX_DISPLAYED_RPM))
 	var rpm_range_val   : int   = int(abs(1 - max_rpm_ratio) * 100)
-	print(max_rpm_ratio)
-	print(rpm_range_val)
 	_rpm_range.value = rpm_range_val
 
 func get_debug_vehicle() -> Vehicle:
