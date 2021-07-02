c663d3bf9f8b8bcb01732b1178fa1df78b6c0a73
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-07-02 16:49:23 +0200

    Finalize GameSettings rewrite

--- scripts/GameSettings.gd
+++ scripts/GameSettings.gd
@@ -58,8 +58,7 @@ func _ready() -> void:
 		user_outdated = true
 	elif user_edition > default_edition:
 		user_configured = false
-		push_error( "GameSettings: Bad configuration file!" + \
-					"              A new configuration will be created.")
+		push_error( "GameSettings: Bad configuration file! A new configuration will be created.")
 		var folder : Directory = Directory.new()
 		folder.rename(USER_CONFIG_FILE, USER_BACKUP_FILE)
 	
@@ -78,14 +77,20 @@ func _ready() -> void:
 				if ! _config.has_section_key(section, setting):
 					var default_value = default_config.get_value(section, setting)
 					_config.set_value(section, setting, default_value)
+		_config.set_value("Meta", "ConfigFileEdition", default_edition)
 		_config.save(USER_CONFIG_FILE)
+
+func reload_settings() -> void:
+	if _config.load(USER_CONFIG_FILE) != OK:
+		push_error("GameSettings: Reloading Settings failed!")
+	emit_signal("setting_changed")
 
 # - Retrieves a setting -
 func get_setting(section : String, setting : String):
 	if _config.has_section_key(section, setting):
 		return _config.get_value(section, setting)
 	else:
-		push_error("GameSetting: Setting \"" + section + "/" + setting + "\" could not be found")
+		push_error("GameSettings: Setting \"" + section + "/" + setting + "\" could not be found")
 
 # - Saves a setting -
 func set_setting(section : String, setting : String, value) -> void:
@@ -94,5 +99,5 @@ func set_setting(section : String, setting : String, value) -> void:
 		_config.save(USER_CONFIG_FILE)
 		emit_signal("setting_changed")
 	else:
-		push_error("GameSetting: Setting \"" + section + "/" + setting + "\" could not be saved")
+		push_error("GameSettings: Setting \"" + section + "/" + setting + "\" could not be saved")
 	
