659c4d5e300c1cb2bfc73042fc0d0b208d045b9c
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-07-20 17:52:57 +0200

    Barrier: Only break when middle part was hit
    This was (cause of an bug) already the behavior, so we make it a feature!

--- scripts/props/road/Barrier.gd
+++ scripts/props/road/Barrier.gd
@@ -14,24 +14,19 @@ onready var right_joint : Generic6DOFJoint = $RightToBeamJoint
 # -- Functions --
 
 # - Break the barrier apart -
-func break_barrier(_body : PhysicsBody, affected : int):
+func break_barrier(_body : PhysicsBody):
 	if _body != null:
-		# Check which part was hit
-		var detach_left  : bool = true if affected >= 1 else false
-		var detach_right : bool = true if affected <= 1 else false
-		
-		# Detach parts
-		if detach_left:
-			left_joint.set_flag_x(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
-			left_joint.set_flag_x(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
-			left_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
-			left_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
-			left_joint.set_flag_z(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
-			left_joint.set_flag_z(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
-		if detach_right:
-			right_joint.set_flag_x(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
-			right_joint.set_flag_x(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
-			right_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
-			right_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
-			right_joint.set_flag_z(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
-			right_joint.set_flag_z(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
+		# Detach left leg
+		left_joint.set_flag_x(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
+		left_joint.set_flag_x(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
+		left_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
+		left_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
+		left_joint.set_flag_z(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
+		left_joint.set_flag_z(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
+		# Detach right leg
+		right_joint.set_flag_x(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
+		right_joint.set_flag_x(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
+		right_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
+		right_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
+		right_joint.set_flag_z(Generic6DOFJoint.FLAG_ENABLE_LINEAR_LIMIT,  false)
+		right_joint.set_flag_z(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_LIMIT, false)
