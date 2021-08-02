c03a1639196ee36d3061fb4ad7c8df70bbfa4ee5
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-08-02 15:30:11 +0200

    VehicleSelect: Prepare for inclusion in "Main"

--- scripts/game/VehicleSelect.gd
+++ scripts/game/VehicleSelect.gd
@@ -15,6 +15,7 @@ const MOVE_THRESHOLD : float = 0.02
 
 # - All vehicles to be selected -
 export (Dictionary) var VehicleViews = Dictionary()
+export (bool)       var DisplayHUD   setget set_display_hud, get_display_hud
 
 # -- Variables --
 
@@ -22,24 +23,23 @@ export (Dictionary) var VehicleViews = Dictionary()
 var vehicle_pool  : Array = Array()
 var viewport_pool : Array = Array()
 
-# - Internal nodes -
-onready var camera_node : Camera = $Camera
-
 # - HUD nodes -
-onready var back_button : Button = $HUD/Selector/BackButton
-onready var next_button : Button = $HUD/Selector/NextButton
-onready var name_label  : Label  = $HUD/Selector/NameBox/VehicleName
-onready var info_label  : Label  = $HUD/SideContainer/InfoContainer/InfoLabel
+onready var back_button  : Button       = $HUD/Selector/BackButton
+onready var next_button  : Button       = $HUD/Selector/NextButton
+onready var name_label   : Label        = $HUD/Selector/NameBox/VehicleName
+onready var info_label   : Label        = $HUD/SideContainer/InfoContainer/InfoLabel
+onready var color_option : OptionButton = $HUD/SideContainer/ColorOption
 
 # - Runtime variables -
 var selected_vehicle   : int  = 0
 var interpolate_view   : bool = false
 var viewport_transform : Transform
+var camera_transform   : Transform
 
 # -- Functions --
 
 # - Runs at startup -
-func _ready():
+func _ready() -> void:
 	# Detect all vehicles and check for CameraPoint
 	for vehicle in VehicleViews.keys():
 		if ! vehicle is NodePath or ! VehicleViews[vehicle] is NodePath:
@@ -53,7 +53,7 @@ func _ready():
 	select_vehicle(0, true)
 
 # - Moves the selection -
-func move_selection(index: int):
+func move_selection(index: int) -> void:
 	var new_select = selected_vehicle + index
 	if new_select < 0:
 		new_select = 0
@@ -62,12 +62,12 @@ func move_selection(index: int):
 	select_vehicle(new_select)
 
 # - Select a new vehicle -
-func select_vehicle(index: int, fast: bool = false):
+func select_vehicle(index: int, fast: bool = false) -> void:
 	selected_vehicle = index
 	# Set camera to viewport
 	viewport_transform = viewport_pool[selected_vehicle].global_transform
 	if fast:
-		camera_node.global_transform = viewport_transform
+		camera_transform = viewport_transform
 	else:
 		interpolate_view = true
 	# Set HUD elements
@@ -78,12 +78,11 @@ func select_vehicle(index: int, fast: bool = false):
 	info_label.text = vehicle_pool[selected_vehicle].VehicleInfo
 
 # - Interpolates camera -
-func _process(delta):
+func _process(delta: float) -> void:
 	if interpolate_view:
-		camera_node.global_transform = \
-			camera_node.global_transform.interpolate_with(
-				viewport_transform, MOVE_SPEED * delta)
-		if _transforms_close(camera_node.global_transform, viewport_transform, MOVE_THRESHOLD):
+		camera_transform = camera_transform.interpolate_with(
+			viewport_transform, MOVE_SPEED * delta)
+		if _transforms_close(camera_transform, viewport_transform, MOVE_THRESHOLD):
 			interpolate_view = false
 
 # - If a transform is near of another -
@@ -94,3 +93,21 @@ func _transforms_close(a : Transform, b : Transform, threshold : float) -> bool:
 		and (a.basis.z - b.basis.z).length() < threshold
 		and (a.origin  - b.origin).length()  < threshold
 	)
+
+# - Returns the wanted camera transform -
+func wanted_transform() -> Transform:
+	return camera_transform
+
+# - DisplayHUD property -
+func get_display_hud() -> bool:
+	return $HUD.visible
+
+func set_display_hud(displaying: bool) -> void:
+	$HUD.visible = displaying
+	if displaying:
+		var vehicle_lenght : int = vehicle_pool.size() - 1
+		back_button.disabled = true if selected_vehicle == 0 else false
+		next_button.disabled = true if selected_vehicle == vehicle_lenght else false
+	else:
+		back_button.disabled = false
+		next_button.disabled = false
