58774095896064070a7a6fe3c4de8cd36655b2af
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-08-02 13:10:01 +0200

    VehicleSelect: Add init

--- scripts/game/VehicleSelect.gd
+++ scripts/game/VehicleSelect.gd
@@ -7,4 +7,29 @@ extends Spatial
 
 # -- Properties --
 
+# - All vehicles to be selected -
+export (Dictionary) var VehicleViews = Dictionary()
 
+# -- Variables --
+
+# - Internal nodes
+var vehicle_pool : Array = Array()
+
+# - HUD nodes -
+onready var back_button : Button = $HUD/Selector/BackButton
+onready var next_button : Button = $HUD/Selector/NextButton
+
+# -- Functions --
+
+# - Runs at startup -
+func _ready():
+	# Detect all vehicles and check for CameraPoint
+	for vehicle in VehicleViews.keys():
+		if ! vehicle is NodePath or ! VehicleViews[vehicle] is NodePath:
+			push_error("VehicleSelect: VehicleViews contains non-NodePath!")
+			return
+		if ! get_node(vehicle) is Vehicle or ! get_node(VehicleViews[vehicle]) is Position3D:
+			push_error("VehicleSelect: No valid format of VehicleViews (Correct would be [Vehicle : Position3D])")
+			return
+		var vehicle_node  : Vehicle    = get_node(vehicle)
+		var viewport_node : Position3D = get_node(VehicleViews[vehicle])
