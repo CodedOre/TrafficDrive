fe89ef8491cca50aa71b6e606bd94a110a5b42cc
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-08-03 15:55:53 +0200

    Driving: Add pause handling

--- scripts/game/Driving.gd
+++ scripts/game/Driving.gd
@@ -16,18 +16,24 @@ onready var gimbalcam   : Spatial    = $GimbalCamera
 onready var vehicleinfo : Control    = $VehicleInfo
 onready var debugscreen : Control    = $DebugScreen
 
+# - Runtime states -
+var paused : bool = false
+
 # -- Functions --
 
-func _init(vehicle_path: String):
+func _init(vehicle_path: String = ""):
 	driven_vehicle_path = vehicle_path
 
 # - Runs at startup -
 func _ready() -> void:
-	if driven_vehicle_path != null:
+	if driven_vehicle_path != null and driven_vehicle_path != "":
 		setup_driving(driven_vehicle_path)
+	else:
+		push_error("Driving: Can't initialize without an path to a vehicle!")
+		setup_driving("res://vehicles/scenes/Police.tscn")
 
 func setup_driving(vehicle_path: String):
-	if vehicle_path == null:
+	if vehicle_path == null or vehicle_path == "":
 		push_error("Driving: Can't initialize without an path to a vehicle!")
 	# Load the selected vehicle
 	var vehicle_scene   : PackedScene = load(vehicle_path)
@@ -44,3 +50,20 @@ func setup_driving(vehicle_path: String):
 	spawned_vehicle.Controlled = true
 	spawned_vehicle.Running = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# - Runs every frame -
+func _process(_delta):
+	# Check for input to pause the game
+	if Input.is_action_just_pressed("ui_cancel"):
+		paused = ! paused
+		_manage_pause()
+
+# - Manages the pause -
+func _manage_pause():
+	# Set mouse capture
+	if paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	# Pause the tree and show the menu
+	get_tree().paused = paused
