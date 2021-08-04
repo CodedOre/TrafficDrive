ce2c36609a51a6a4a3c65bb2b697d97290f9eaa8
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-08-04 17:23:10 +0200

    SettingsMenu: Add UI for Keybind changes

--- scripts/hud/menu/SettingsMenu.gd
+++ scripts/hud/menu/SettingsMenu.gd
@@ -8,7 +8,7 @@ extends CenterContainer
 # -- Enums --
 
 # - The tabs of the menu -
-enum SettingTabs {GRAPHICS, GAMEPLAY, INPUT}
+enum SettingTabs {GRAPHICS, GAMEPLAY, INPUT, KEYS}
 
 # -- Variables --
 
@@ -17,12 +17,32 @@ var current_tab # SettingTabs
 onready var tab_containers : Dictionary = {
 	SettingTabs.GRAPHICS: $MenuBox/ContentContainer/Graphics,
 	SettingTabs.GAMEPLAY: $MenuBox/ContentContainer/Gameplay,
-	SettingTabs.INPUT:    $MenuBox/ContentContainer/Input
+	SettingTabs.INPUT:    $MenuBox/ContentContainer/Input,
+	SettingTabs.KEYS:     $MenuBox/ContentContainer/Keys
 }
 onready var tab_selectors : Dictionary = {
 	SettingTabs.GRAPHICS: $MenuBox/TabsContainer/GraphicsTab,
 	SettingTabs.GAMEPLAY: $MenuBox/TabsContainer/GameplayTab,
-	SettingTabs.INPUT:    $MenuBox/TabsContainer/InputTab
+	SettingTabs.INPUT:    $MenuBox/TabsContainer/InputTab,
+	SettingTabs.KEYS:     $MenuBox/TabsContainer/KeysTab
+}
+
+# - Input actions -
+var changeable_inputs : Dictionary = {
+	"vehicle_movement_forward": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/ForwardButton,
+	"vehicle_movement_backward": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/BackwardsButton,
+	"vehicle_movement_left": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/SteerLeftButton,
+	"vehicle_movement_right": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/SteerRightButton,
+	"vehicle_gear_up": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/GearUpButton,
+	"vehicle_gear_down": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/GearDownButton,
+	"vehicle_light_night": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/HeadlightsButton,
+	"vehicle_light_turn_left": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/TurnLeftButton,
+	"vehicle_light_turn_right": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/TurnRightButton,
+	"vehicle_light_hazards": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/HazardsButton,
+	"vehicle_change_camera": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/CameraButton,
+	"vehicle_set_cruise_control": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/CruiseControlButton,
+	"vehicle_increase_cruise_control": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/CruiseUpButton,
+	"vehicle_decrease_cruise_control": $MenuBox/ContentContainer/Keys/ScrollContainer/KeysGrid/CruiseDownButton
 }
 
 # - Graphics nodes -
@@ -34,6 +54,14 @@ onready var vehicle_lights  : OptionButton = $MenuBox/ContentContainer/Graphics/
 onready var automatic_option : CheckBox = $MenuBox/ContentContainer/Gameplay/AutoGearCheck
 onready var imperial_option  : CheckBox = $MenuBox/ContentContainer/Gameplay/ImperialCheck
 onready var mirror_option    : CheckBox = $MenuBox/ContentContainer/Gameplay/MirrorCheck
+
+# - Input nodes -
+onready var invert_option      : CheckBox = $MenuBox/ContentContainer/Input/InvertCheck
+onready var sensitivity_value  : Label    = $MenuBox/ContentContainer/Input/SensitivityBox/SensitivityValue
+onready var sensitivity_slider : HSlider  = $MenuBox/ContentContainer/Input/SensitivityBox/SensitivitySlider
+
+# - Runtime variables -
+var changing_input : bool = false
 
 # -- Signals --
 signal close_options()
@@ -70,6 +98,9 @@ func _load_settings() -> void:
 	automatic_option.pressed = GameSettings.get_setting("Gameplay", "SwitchGearsAutomatically")
 	imperial_option.pressed = GameSettings.get_setting("Gameplay", "UseImperialUnits")
 	mirror_option.pressed = GameSettings.get_setting("Gameplay", "OuterMirrorActive")
+	invert_option.pressed = GameSettings.get_setting("Input", "MouseXInverted")
+	sensitivity_slider.value = GameSettings.get_setting("Input", "MouseSensitivity")
+	sensitivity_value.text = str(sensitivity_slider.value)
 
 # - Sets the settings to the wanted value -
 func _set_display_options(setting : int) -> void:
@@ -84,7 +115,18 @@ func _set_imperial_units(setting : bool) -> void:
 	GameSettings.set_setting("Gameplay", "UseImperialUnits", setting)
 func _set_mirror_option(setting : bool) -> void:
 	GameSettings.set_setting("Gameplay", "OuterMirrorActive", setting)
+func _set_invert_option(setting : bool) -> void:
+	GameSettings.set_setting("Input", "MouseXInverted", setting)
+func _set_mouse_sensitivity(setting : float) -> void:
+	GameSettings.set_setting("Input", "MouseSensitivity", setting)
 
 # - Calls for an complete settings reset -
 func _reset_settings() -> void:
 	GameSettings.reset_settings()
+
+# - Sets the values for the keybind buttons -
+func _display_set_keys() -> void:
+	for action in changeable_inputs.keys():
+		var input_button : Button = changeable_inputs[action]
+		var input_event  : int    = GameSettings.get_setting("Keys", action)
+		input_button.text = OS.get_scancode_string(input_event)
