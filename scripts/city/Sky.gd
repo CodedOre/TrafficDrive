9a4af1d01baab40fc61fdd55b4d666ff9e50bec5
CodedOre <47981497+CodedOre@users.noreply.github.com>
2021-08-10 18:32:22 +0200

    Sky: Adapt project code style

--- scripts/city/Sky.gd
+++ scripts/city/Sky.gd
@@ -1,14 +1,26 @@
+# Copyright 2019, Tapio Pyrhönen
+
+# --- Sky script ---
+# Responsible for the mesh displaying the Sky shader.
+
 extends MeshInstance
 
-export (NodePath) var sun # Directional light for the sun
-var use_only_lod = false # Not used
+# -- Properties -
 
+# - Directional light for the sun -
+export (NodePath) var sun
+
+# -- Functions --
+
+# - Runs at startup -
 func _ready():
 	sun = get_node(sun)
 
+# - Runs at every frame -
 func _process(delta):
 	call_deferred("follow_camera")
 
+# - Updates the mesh to cover the complete viewport -
 func follow_camera():
 	var cam = get_viewport().get_camera()
 	transform = cam.global_transform
@@ -16,11 +28,13 @@ func follow_camera():
 	scale = Vector3(cam.far * 4.0, cam.far * 3.0, 1.0)
 	material_override.set_shader_param("camera_y", cam.global_transform.origin.y)
 	material_override.set_shader_param("sun_dir", sun.global_transform.basis.z)
-	
+
+# - Sets the color of the clouds -
 func set_cloud_colors(cloud_color, lining_color, horizon_color):
 	material_override.set_shader_param("cloud_color", cloud_color)
 	material_override.set_shader_param("lining_color", lining_color)
 	material_override.set_shader_param("horizon_color", horizon_color)
 
+# - Sets how much clouds there are -
 func set_cloudiness(cloudiness):
 	material_override.set_shader_param("cloudiness", cloudiness)
